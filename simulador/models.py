e6543926bdd6ab4678c6a1941330061c03273e8a
Arthur_XC14 <arthurvfbarros@gmail.com>
2026-08-07 17:14:11 -0300

    adicionando pilotos do grid atual

--- simulador/models.py
+++ simulador/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Piloto(models.Model):
+        nome = models.CharField(max_length=50)
+        sobrenome = models.CharField(max_length=50)
+        numero = models.CharField(max_length=5, blank = True, null = True)
+        nacionalidade = models.CharField(max_length=50)
+
+        def __str__(self):
+            return f"{self.nome} {self.sobrenome} - {self.numero}"
