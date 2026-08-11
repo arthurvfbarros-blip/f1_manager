490d7316c8bbcad373ceba5386e96153af863dd5
Arthur_XC14 <arthurvfbarros@gmail.com>
2026-08-11 19:48:10 -0300

    adição de equipes, patrocinadores e fornecedores de motor

--- simulador/models.py
+++ simulador/models.py
@@ -17,3 +17,20 @@ class Pista(models.Model):
 
       def __str__(self):
             return f"{self.nome}, pais: {self.pais}, tamanho:{self.tamanho}, voltas: {self.quantidade_voltas}"
+
+class FornecedorMotor(models.Model):
+      nome = models.CharField(max_length=50)
+      potencia = models.IntegerField()
+      confiabilidade = models.IntegerField()
+      preco = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Patrocinador(models.Model):
+      nome = models.CharField(max_length=100)
+      pagamento_por_corrida = models.DecimalField(max_digits=10, decimal_places=2)
+      meta_posicao = models.IntegerField()
+
+class Equipe(models.Model):
+      nome = models.CharField(max_length=100)
+      orcamento = models.DecimalField(max_digits=15, decimal_places=2)
+      motor = models.ForeignKey(FornecedorMotor, on_delete=models.SET_NULL, null=True)
+      patrocinadores = models.ManyToManyField(Patrocinador)     
